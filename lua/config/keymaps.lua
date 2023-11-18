defe93aff3ed288c2c9c505ff5d17cf19249f854
Bac Nguyen <nxbac.dev@gmail.com>
2023-11-18 14:17:16 +0700

    init lazyvim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,26 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- local keymap = vim.keymap
+--
+-- -- Increment/decrement
+-- keymap.set('n', '+', '<C-a>')
+-- keymap.set('n', '-', '<C-x>')
+--
+-- -- New tab
+-- keymap.set('n', 'te', ':tabedit <CR>')
+-- -- Move tab
+-- keymap.set('n', '<Tab>', ':tabnext <CR>')
+-- keymap.set('n', 'S-<Tab>', ':tabprev <CR>')
+-- -- Close tab
+-- keymap.set('n', 'tq', ':quit <CR>')
+-- -- Split window
+-- keymap.set('n', 'ss', ':split<Return><C-w>w')
+-- keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+-- -- Move window
+-- keymap.set('n', '<Space>', '<C-w>w')
+-- keymap.set('', 'sh', '<C-w>h')
+-- keymap.set('', 'sk', '<C-w>k')
+-- keymap.set('', 'sj', '<C-w>j')
+-- keymap.set('', 'sl', '<C-w>l')
