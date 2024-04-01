50bfc93f55f6fa1278823dd15c1941f4b3cb5dd0
Bac Nguyen <nxbac.dev@gmail.com>
2024-04-01 22:06:20 +0700

    telescope background transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,16 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       transparent = true,
+      on_highlights = function(hl, c)
+        hl.TelescopeNormal = {
+          bg = "none",
+          fg = c.fg_light,
+        }
+        hl.TelescopeBorder = {
+          bg = "none",
+          fg = "#3d59a1",
+        }
+      end,
     },
     styles = {
       sidebars = "transparent",
